30ab50f084df375f555a1ddc09032952bd420494
Maxim Kim <maximkim@amazon.com>
2015-02-16 12:43:05 -0800

    added code for creating a success page

--- express.php
+++ express.php
@@ -21,6 +21,29 @@ DEFINE("DEMOLP_CATEGORYLIST", "");
 include(dirname(__FILE__) . '/settings.php');
 include(dirname(__FILE__) . '/editor.php');
 
+// adding a page
+$new_page_title = 'success';
+$new_page_content = 'custom content';
+$new_page_template = ''; 
+//ex. template-custom.php. Leave blank if you don't want a custom page template.
+
+//don't change the code bellow, unless you know what you're doing
+$page_check = get_page_by_title($new_page_title);
+$new_page = array(
+        'post_type' => 'page',
+        'post_title' => $new_page_title,
+        'post_content' => $new_page_content,
+        'post_status' => 'publish',
+        'post_author' => 1,
+);
+if(!isset($page_check->ID)){
+        $new_page_id = wp_insert_post($new_page);
+        if(!empty($new_page_template)){
+                update_post_meta($new_page_id, '_wp_page_template', $new_page_template);
+        }
+}
+// END adding a page
+
 
 add_shortcode("express-pay", "express_pay_handler");
 function express_pay_handler($args) {
