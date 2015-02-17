6ade1cd123c1a914b05a057e7e0b421e1527b379
Maxim Kim <maxim@maximkim.com>
2015-02-17 00:15:14 +0000

    added email and email notification to the admin page

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 add_action('admin_menu', 'amzn_plugin_settings');
 function amzn_plugin_settings() {
-    add_menu_page('Amzn Settings', 'Amzn Settings', 'administrator', 'amzn_settings', 'amzn_display_settings');
+    add_menu_page('Amazon Settings', 'Amazon Settings', 'administrator', 'amzn_settings', 'amzn_display_settings');
 }
 
 function amzn_display_settings() {
@@ -10,6 +10,8 @@ function amzn_display_settings() {
     $accessKey = (get_option('amzn_access_key') != '') ? get_option('amzn_access_key') : 'AKIAIXYYATQC6HR75ZWA';
     $secretKey = (get_option('amzn_secret_key') != '') ? get_option('amzn_secret_key') : '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';
     $returnURL = (get_option('amzn_return_url') != '') ? get_option('amzn_return_url') : 'https://54.201.197.139';
+    $email = (get_option('amzn_email') != '') ? get_option('amzn_email') : 'maximkim@amazon.com';
+    $emailNotn = (get_option('amzn_email_notn') == 'enabled') ? 'checked' : '';
     $html = '</pre>
 			<div class="wrap"><form action="options.php" method="post" name="options">
 			<h2>Select Your Settings</h2>
@@ -36,10 +38,17 @@ function amzn_display_settings() {
 			<td scope="row" align="left">
 			 <label>Default return URL</label><input type="text" name="amzn_return_url" value="' . $returnURL . '" /></td>
 			</tr>
+			<tr>
+                          <td scope="row" align="left"><label>Enable email notifications?</label><input type="checkbox" name="amzn_email_notn" value="enabled" ' . $emailNotn . '/></td>
+			</tr>
+			<tr>
+			<td scope="row" align="left">
+			 <label>Email address</label><input type="text" name="amzn_email" value="' . $email . '" /></td>
+			</tr>
 			</tbody>
 			</table>
 			 <input type="hidden" name="action" value="update" />
-			 <input type="hidden" name="page_options" value="amzn_seller_id,amzn_lwa_client_id,amzn_access_key,amzn_secret_key,amzn_return_url" />
+			 <input type="hidden" name="page_options" value="amzn_seller_id,amzn_lwa_client_id,amzn_access_key,amzn_secret_key,amzn_return_url,amzn_email_notn,amzn_email" />
 			 <input type="submit" name="Submit" value="Update" /></form></div>
 			<pre>';
     echo $html;
