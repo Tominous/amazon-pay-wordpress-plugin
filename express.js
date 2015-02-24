13d9932d70ce3057b5f40f79b9a7a357c3213ea6
Maxim Kim <maxim@maximkim.com>
2015-02-24 21:29:46 +0000

    formatted express.js

--- express.js
+++ express.js
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2015 Amazon.com, Inc., or its Affiliates.
+ */
+
 function amznexpressplugin() {
 	return sc_text;
 }
@@ -9,29 +13,29 @@ function amznexpressplugin() {
 		init : function(ed, url){
 			ed.addButton('amznexpressplugin', {
 				title : 'Insert Express Checkout Shortcode',
-				onclick : function() {
-					ed.execCommand(
-						'mceInsertContent',
-						false,
-							amznexpressplugin()
-						);
-				},
-				image : sc_img
+			onclick : function() {
+				ed.execCommand(
+					'mceInsertContent',
+					false,
+					amznexpressplugin()
+					);
+			},
+			image : sc_img
 			});
 		},
 
-		getInfo : function() {
-			return {
-				longname : 'Amazon Express Checkout TinyMCE Shortcode plugin',
-				author : 'Maxim Kim',
-				authorurl : 'http://payments.amazon.com',
-				infourl : 'http://payments.amazon.com',
-				version : "1.0"
-			};
-		}
+	getInfo : function() {
+		return {
+			longname : 'Pay with Amazon Express TinyMCE Shortcode plugin',
+	author : 'Pay with Amazon',
+	authorurl : 'http://payments.amazon.com',
+	infourl : 'http://payments.amazon.com',
+	version : "1.0"
+		};
+	}
 
 	});
 
 	tinymce.PluginManager.add('amznexpressplugin', tinymce.plugins.amznexpressplugin);
-	
+
 })();	
