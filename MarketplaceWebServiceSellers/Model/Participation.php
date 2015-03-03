fec35c096e5ec7f56429a4fcbba7b907dd0b55f5
Maxim Kim <maxim@maximkim.com>
2015-03-03 21:44:04 +0000

    changed license to MIT in the PHP SDK file headers

--- MarketplaceWebServiceSellers/Model/Participation.php
+++ MarketplaceWebServiceSellers/Model/Participation.php
@@ -1,10 +1,10 @@
 <?php
 /*******************************************************************************
  * Copyright 2009-2014 Amazon Services. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * Licensed under the MIT License (the "License"); 
  *
  * You may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at: http://aws.amazon.com/apache2.0
+ * You may obtain a copy of the License at: http://opensource.org/licenses/MIT
  * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
  * specific language governing permissions and limitations under the License.
